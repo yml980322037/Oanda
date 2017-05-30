763fd2f52eca61d0b650da9bfc6f4f6f142d3b66
sh31 <s.hassan31@icloud.com>
2017-05-30 13:38:50 +0100

    implemented stop loss/take profit logic

--- risk.py
+++ risk.py
@@ -9,21 +9,31 @@ class Risk:
         self.riskpct = riskpct/100
 
     def calculate_risk(self, event_object):
-        ev = event.Event('trade')
-        ev.df = event_object.df
-        ev.sl = self.sl_tp_calculator(ev.df, event_object.signal)
-
+        ev = event_object
+        ev.type = 'trade'
+        ev.sl, ev.tp = self.sl_tp_calculator(ev.df, event_object.signal)
 
         return ev
 
         
     def sl_tp_calculator(self, dataframe, signal):
         if signal == 'buy':
-            return max(dataframe[-6:]['close'])
+            price = dataframe.iloc[-1]['close']
+            sl = min(dataframe[-6:]['low'])
+            diff = (price - sl)
+            tp = round(price + (diff * 1.5), 3)
+            return (sl, tp)
+
         elif signal == 'sell':
-            return min(dataframe[-6:]['close'])
+            price = dataframe.iloc[-1]['close']
+            sl = max(dataframe[-6:]['high'])
+            diff = (sl - price)
+            tp = round(price - (diff * 1.5), 3)
+            return (sl, tp)
+        else:
+            return (0,0)
             
-    def position_size_calculator(self, instrument, balance, riskpct, stoploss):
+    def position_size_calculator(self, balance, riskpct, stoploss):
         return ((balance * (riskpct/100) / stoploss) / 0.00008817) #change to retrieve live rate on call
         
     def margin_requirement(self, lotsize, marginpct):
