09487762d495c16b86a75072ac9f89f8ec1035ab
sh31 <s.hassan31@icloud.com>
2017-06-01 08:56:16 +0100

    layout

--- strategy.py
+++ strategy.py
@@ -13,6 +13,7 @@ class Strategy:
 		an.add_2_sma_av(three_hour_df, 2, 7)
 		an.add_state(three_hour_df, 'SMA2', 'SMA7')
 		three_hour_trend = three_hour_df.iloc[-1]['State']
+		ev.threehrdf = three_hour_df
 
 
 		if ev.df.iloc[-1]['State'] != ev.df.iloc[-2]['State'] and ev.df.iloc[-1]['State'] == three_hour_trend:
