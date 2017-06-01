bfc8b0853040934ffbc63f39cf617dd0fa9cff6b
sh31 <s.hassan31@icloud.com>
2017-06-01 08:56:57 +0100

    added initialize and update methods

--- portfolio.py
+++ portfolio.py
@@ -5,19 +5,36 @@ class Portfolio:
     
     def __init__(self):
         #initialist balance, margin requirement, pull existing open trades.
-        self.balance = None #Pull from API
-    
-    def view_open_trades():
-        pass
-    
+        self.balance, self.currency, self.margin_available, self.margin_used = self.initialize_portfolio()
+
+
     def check_risk():
         #call risk module
         pass
     
+
+    def view_open_trades():
+        pass
+    
+    
     def open_trade(instrument, lotsize, stoploss, takeprofit):
         pass
+
 
     def account_summary(self):
         self.url = 'https://api-fxpractice.oanda.com/v3/accounts/{}'.format(config.account_id)
         self.headers = {'Authorization' : 'Bearer ' + config.access_token}
-        return requests.get(self.url, headers = self.headers)
+        req = requests.get(self.url, headers = self.headers)
+        json = req.json()
+        return json['account']
+
+
+    def initialize_portfolio(self):
+        summary = self.account_summary()
+        return (summary['balance'], summary['currency'], summary['marginAvailable'], summary['marginUsed'])
+
+
+    def update_margins(self):
+        summary = self.account_summary()
+        return (summary['marginAvailable'], summary['marginUsed'])
+
