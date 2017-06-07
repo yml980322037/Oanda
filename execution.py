01557eecfbd005879ed0141b5b88604c152614ea
sh31 <s.hassan31@icloud.com>
2017-06-07 18:12:16 +0100

    implemented functions

---
+++ execution.py
@@ -0,0 +1,98 @@
+import config
+import requests
+import json
+from datetime import datetime
+import mysql.connector
+
+class Execution:
+
+	def __init__(self, ticker):
+		self.ticker = ticker
+		self.last_trade_ID = '-1'
+
+
+	def order(self, units, sl, tp):
+		url = 'https://api-fxpractice.oanda.com/v3/accounts/{}/orders'.format(config.account_id)
+		headers = {'Authorization' : 'Bearer ' + config.access_token, 'Content-Type': 'application/json', 'Accept-Datetime-Format': 'UNIX'}
+		stop_loss = {'price': str(sl)}
+		take_profit = {'price': str(tp)}
+		data = {'order':{'units': str(units), 'instrument': self.ticker, 'timeInForce': 'FOK',
+		'type': 'MARKET', 'stopLossOnFill': stop_loss, 'takeProfitOnFill': take_profit, 'positionFill': 'DEFAULT'}}
+		req = requests.post(url, headers = headers, data = json.dumps(data))
+		json_data = req.json()
+		self.last_trade_ID = json_data['orderFillTransaction']['id']
+		return json_data
+
+		#INCOMPLETE
+	def close_last_trade(self):
+		url = 'https://api-fxpractice.oanda.com/v3/accounts/{}/trades/{}/close'.format(config.account_id, self.last_trade_ID)
+		headers = {'Authorization' : 'Bearer ' + config.access_token, 'Content-Type': 'application/json', 'Accept-Datetime-Format': 'UNIX'}
+
+
+	def log_to_db(self, order_response):
+		# If statements to call correct method
+		for key, value in order_response.items():
+			if key == 'orderCreateTransaction':
+				self.log_order_create_transaction(value)
+			elif key == 'orderFillTransaction':
+				self.log_order_fill_transaction(value)
+			elif key == 'orderCancelTransaction':
+				self.log_order_cancel_transaction(value)
+		
+
+
+	def log_order_create_transaction(self,json_data):
+		self.time_reformatter(json_data)
+		json_data['stopLossOnFill'] = json_data['stopLossOnFill']['price']
+		json_data['takeProfitOnFill'] = json_data['takeProfitOnFill']['price']
+		add_row = ("INSERT INTO Log"
+			"(id, batch_id, time, type, reason, units, stop_loss, take_profit) "
+			"VALUES (%(id)s, %(batchID)s, %(time)s, %(type)s, %(reason)s, "
+			"%(units)s, %(stopLossOnFill)s, %(takeProfitOnFill)s)")
+		self.execute_db_query(add_row, json_data)
+
+	def log_order_fill_transaction(self,json_data):
+		data = json_data
+		data['tradeOpened'] = '0'
+		self.time_reformatter(data)
+		add_row = ("INSERT INTO Log"
+			"(id, batch_id, time, type, reason, price, units, financing, account_balance) "
+			"VALUES (%(id)s, %(batchID)s, %(time)s, %(type)s, %(reason)s, "
+			"%(price)s, %(units)s, %(financing)s, %(accountBalance)s)")
+		self.execute_db_query(add_row, data)
+
+
+	def log_order_cancel_transaction(self, json_data):
+		data = json_data
+		self.time_reformatter(data)
+		add_row = ("INSERT INTO Log"
+			"(id, batch_id, time, type, reason) "
+			"VALUES (%(id)s, %(batchID)s, %(time)s, %(type)s, %(reason)s)")
+		self.execute_db_query(add_row, data)
+
+
+	def execute_db_query(self, query, data):
+		cnx = mysql.connector.connect(user=config.sql_user_sourcefeed, password=config.sql_password_sourcefeed,
+			host=config.sql_host, database='USDJPY')
+		cursor = cnx.cursor()
+		cursor.execute(query, data)
+		cnx.commit()
+		cursor.close()
+		cnx.close()
+
+
+	def create_log_table(self):
+		create = ("CREATE TABLE IF NOT EXISTS Log"
+			"(id INTEGER UNIQUE, batch_id INTEGER, time DATETIME, type CHAR(15), reason CHAR(15), price DECIMAL(6,3), "
+			" units INTEGER, pl DECIMAL(8,3), financing DECIMAL(8,3), stop_loss DECIMAL(6,3), take_profit DECIMAL(6,3), "
+			"account_balance DECIMAL(15,5))")
+		cnx = mysql.connector.connect(user=config.sql_user_sourcefeed, password=config.sql_password_sourcefeed,
+			host=config.sql_host, database='USDJPY')
+		cursor = cnx.cursor()
+		cursor.execute(create)
+		cursor.close()
+		cnx.close()
+
+
+	def time_reformatter(self, json_data):
+		json_data['time'] = datetime.strftime(datetime.fromtimestamp(float(json_data['time'][:10])),'%Y-%m-%d %H:%M:%S')
--- portfolio.py
+++ portfolio.py
@@ -5,36 +5,39 @@ class Portfolio:
     
     def __init__(self):
         #initialist balance, margin requirement, pull existing open trades.
-        self.balance, self.currency, self.margin_available, self.margin_used = self.initialize_portfolio()
+        self.update()
+        self.start_balance = self.balance
+        self.percentage_change()
 
 
-    def check_risk():
-        #call risk module
-        pass
+    def check_order(self, event_object):
+        ev = event_object
+        self.update()
+        self.percentage_change()
+        if self.pct_change < -3 or ev.spread > 2 or ev.margin_total > (balance * 0.3) :
+            ev.type = 'none'
+            return ev
+        else:
+            ev.type = 'order'
+            return ev
+        
     
-
-    def view_open_trades():
-        pass
-    
-    
-    def open_trade(instrument, lotsize, stoploss, takeprofit):
-        pass
+    def update(self):
+        summary = self.account_summary()
+        self.balance = float(summary['balance'])
+        self.currency = summary['currency']
+        self.margin_available = summary['marginAvailable']
+        self.margin_used = summary['marginUsed']
+        self.positions = summary['openPositionCount']
 
 
     def account_summary(self):
-        self.url = 'https://api-fxpractice.oanda.com/v3/accounts/{}'.format(config.account_id)
+        self.url = config.url + '/v3/accounts/{}'.format(config.account_id)
         self.headers = {'Authorization' : 'Bearer ' + config.access_token}
         req = requests.get(self.url, headers = self.headers)
         json = req.json()
         return json['account']
 
-
-    def initialize_portfolio(self):
-        summary = self.account_summary()
-        return (summary['balance'], summary['currency'], summary['marginAvailable'], summary['marginUsed'])
-
-
-    def update_margins(self):
-        summary = self.account_summary()
-        return (summary['marginAvailable'], summary['marginUsed'])
-
+    
+    def percentage_change(self):
+        self.pct_change = round((self.balance - self.start_balance) / self.start_balance * 100, 2)
