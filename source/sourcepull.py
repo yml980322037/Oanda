6f4ec51d2b8687ded71f22d4c70249dc893e4701
Shojul Hassan <shojulhassan@Shojuls-MBP.home>
2017-06-20 16:04:01 +0100

    added try except

--- source/sourcepull.py
+++ source/sourcepull.py
@@ -15,23 +15,25 @@ USD_JPY.pull_to_table(datetime.strftime(timenow - timedelta(hours = 6),'%Y-%m-%d
 USD_JPY.pull_to_table(datetime.strftime(timenow - timedelta(days = 3),'%Y-%m-%d %H:%M:%S'),
 	                              datetime.strftime(timenow + timedelta(minutes = 2),'%Y-%m-%d %H:%M:%S'), 'H3')
 
+try:
+	while True:
+		# Change for Daylight Saving Time
+		timenow = datetime.utcnow() + timedelta(hours = 1)
 
-while True:
-	# Change for Daylight Saving Time
-	timenow = datetime.utcnow() + timedelta(hours = 1)
+		if USD_JPY.halted() == False:
+		    try:
+		        USD_JPY.pull_to_table(datetime.strftime(timenow - timedelta(minutes = 50),'%Y-%m-%d %H:%M:%S'),
+		                              datetime.strftime(timenow + timedelta(minutes = 2),'%Y-%m-%d %H:%M:%S'), 'M15')
+		    except ValueError:
+		        print('JSON Value Error')
 
-	if USD_JPY.halted() == False:
-	    try:
-	        USD_JPY.pull_to_table(datetime.strftime(timenow - timedelta(minutes = 50),'%Y-%m-%d %H:%M:%S'),
-	                              datetime.strftime(timenow + timedelta(minutes = 2),'%Y-%m-%d %H:%M:%S'), 'M15')
-	    except ValueError:
-	        print('JSON Value Error')
+		    try:
+		        USD_JPY.pull_to_table(datetime.strftime(timenow - timedelta(hours = 10),'%Y-%m-%d %H:%M:%S'),
+		                              datetime.strftime(timenow + timedelta(minutes = 2),'%Y-%m-%d %H:%M:%S'), 'H3')
+		    except ValueError:
+		        print('JSON Value Error')
 
-	    try:
-	        USD_JPY.pull_to_table(datetime.strftime(timenow - timedelta(hours = 10),'%Y-%m-%d %H:%M:%S'),
-	                              datetime.strftime(timenow + timedelta(minutes = 2),'%Y-%m-%d %H:%M:%S'), 'H3')
-	    except ValueError:
-	        print('JSON Value Error')
-
-	sys.stdout.flush()
-	time.sleep(5)
+		sys.stdout.flush()
+		time.sleep(5)
+except:
+	print('API pull failed')
