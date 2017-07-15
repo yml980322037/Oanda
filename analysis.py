2d9c4b5df1d7b0302697860db5200bc15a7157eb
Shojul Hassan <s.hassan@icloud.com>
2017-07-15 19:26:27 +0100

    added functions

--- analysis.py
+++ analysis.py
@@ -5,6 +5,7 @@ import config
 import mysql.connector
 from datetime import datetime
 from mysql.connector import errorcode
+from decimal import Decimal
 
 sql_host = config.sql_host
 sql_user = config.sql_user_analysis
@@ -16,15 +17,39 @@ account_id = config.account_id
 
 
 # Returns tuple for each sql row.
-def selectlast(granularity, time, rows):
+def selectlast(ticker, granularity, time, rows):
     
     sql = ("SELECT * FROM (SELECT * FROM {} WHERE time <= '{}' "
            "ORDER BY time DESC LIMIT {})a ORDER BY time ASC;".format(granularity, time, rows))
     cnx = mysql.connector.connect(user=sql_user, password=sql_password,
-                             host=sql_host, database='USD_JPY')
+                             host=sql_host, database='{}'.format(ticker))
     cursor = cnx.cursor()
     cursor.execute(sql)
     results = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return results;
+
+def selectdates(ticker, granularity, start, end):
+    
+    sql = ("SELECT * FROM {} WHERE time BETWEEN '{}' AND '{}';".format(granularity, start, end))
+    cnx = mysql.connector.connect(user=sql_user, password=sql_password,
+                             host=sql_host, database='{}'.format(ticker))
+    cursor = cnx.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+    return results;
+
+def selectrow(ticker, granularity, time):
+    
+    sql = ("SELECT * FROM {} WHERE time = '{}';".format(granularity, time))
+    cnx = mysql.connector.connect(user=sql_user, password=sql_password,
+                             host=sql_host, database='{}'.format(ticker))
+    cursor = cnx.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchone()
     cursor.close()
     cnx.close()
     return results;
@@ -41,7 +66,6 @@ def dataframe(sqltable):
 def average_dataframe(sqltable):
     names = ['time', 'openAsk', 'closeAsk', 'lowAsk', 'highAsk', 'openBid', 'closeBid', 'lowBid', 'highBid', 'volume']
     df = DataFrame(sqltable, columns = names)
-    
     df['open'] = df[['openAsk', 'openBid']].mean(axis=1)
     df['high'] = df[['highAsk', 'highBid']].mean(axis=1)
     df['low'] = df[['lowAsk', 'lowBid']].mean(axis=1)
@@ -49,32 +73,68 @@ def average_dataframe(sqltable):
     return df[['time', 'open', 'high', 'low', 'close']]
 
 
-# Adds simple moving average to dataframe
+# Adds simple moving average to average bar dataframe
 def add_sma(df, val):
-    df['SMA{}'.format(val)] = df['closeBid'].rolling(window=val).mean()
+    df['SMA {}'.format(val)] = df['close'].rolling(window=val).mean()
 
 
-def add_2_sma_av(df, val, val2):
+def add_2_sma(df, val, val2):
     if val > val2:
         temp = val
         val = val2
         val2 = temp
-    df['SMA{}'.format(val)] = df['close'].rolling(window=val).mean()
-    df['SMA{}'.format(val2)] = df['close'].rolling(window=val2).mean()
+    df['SMA {}'.format(val)] = df['close'].rolling(window=val).mean()
+    df['SMA {}'.format(val2)] = df['close'].rolling(window=val2).mean()
 
 
-#Returns state of shorter moving average compared to the longer moving average
-def add_state(df, firstsma, secondsma):
+def add_state(df, sma):
     state = 'NA'
-    for i in df.index:
-        if df.ix[i, firstsma] < df.ix[i, secondsma]:
+    col = 'SMA {}'.format(sma)
+    for i in range(2, len(df)):
+        if df[col].iloc[i] < df[col].iloc[i-1]:
             state = 'Below'
-        elif df.ix[i, firstsma] > df.ix[i, secondsma]:
+        elif df[col].iloc[i] > df[col].iloc[i-1]:
             state = 'Above'
         else:
             state = 'NA'
+            
+        df.ix[i, 'State {}'.format(sma)] = state
 
-        df.ix[i, 'State'] = state
+
+#Returns state of shorter moving average compared to the longer moving average
+def compare_state(df, firstsma, secondsma):
+    state = 'NA'
+    for i in df.index:
+        try:
+            if Decimal(df.ix[i, firstsma]) < Decimal(df.ix[i, secondsma]):
+                state = 'Below'
+            elif Decimal(df.ix[i, firstsma]) > Decimal(df.ix[i, secondsma]):
+                state = 'Above'
+            else:
+                state = 'NA'
+
+            df.ix[i, 'State'] = state
+        except:
+            pass
+
+def penultimate_bars_status(df, bars, sma):
+    lst = []
+    for i in range(2, bars+2):
+        lst.append(df['State {}'.format(sma)].iloc[-i])
+    return lst
+
+def penultimate_bars_identical(df, bars, sma):
+    pen_bars_status = penultimate_bars_status(df, bars, sma)
+    return check_equal(pen_bars_status)
+
+def last_bar_status(df, sma):
+    return df['State {}'.format(sma)].iloc[-1]
+
+def check_equal(lst):
+    return lst[1:] == lst[:-1]
+
+def pip_difference(price1, price2):
+    return abs((Decimal(price1) - Decimal(price2)) * 10000)
 
 
 def get_current_rate(instrument):
@@ -97,14 +157,14 @@ def get_spread(instrument):
         item = json_data['prices'][0]
         ask = item['ask']
         bid = item['bid']
-        return round((ask - bid) * 100, 2)
+        return round((ask - bid) * 10000, 2)
 
 def get_balance():
         url = config.url + '/v3/accounts/{}'.format(config.account_id)
         headers = {'Authorization' : 'Bearer ' + config.access_token}
         req = requests.get(url, headers = headers)
         json = req.json()
-        return float(json['account']['balance'])
+        return Decimal(json['account']['balance'])
 
 def get_open_positions():
         url = config.url + '/v3/accounts/{}'.format(config.account_id)
@@ -113,10 +173,10 @@ def get_open_positions():
         json = req.json()
         return int(json['account']['openPositionCount'])
 
-def last_fill():
+def last_fill(ticker):
     sql = ("SELECT time, units FROM Log Where type = 'ORDER_FILL' ORDER BY time DESC LIMIT 1")
     cnx = mysql.connector.connect(user=sql_user, password=sql_password,
-                             host=sql_host, database='USD_JPY')
+                             host=sql_host, database='{}'.format(ticker))
     cursor = cnx.cursor()
     cursor.execute(sql)
     results = cursor.fetchone()
